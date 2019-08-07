f8ceaedac85a815a740dd0ee6a2099f3f001dfba
Cormac Lynch-Collier <cormac.lynch-collier@vistaprint.com>
2019-08-07 18:13:33 -0400

    Published site

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  i: number;
+  skills: string[];
+  speed: number;
+  text: string;
+  skillsInd = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.i = 0;
+    this.skills = ['Angular', 'React', 'NodeJS', '.NETCore', 'WordPress'];
+    this.speed = 200;
+    this.text = '';
+    this.typeWriter();
+    
+  }
+
+  typeWriter() {
+    if (this.i < this.skills[this.skillsInd].length) {
+      this.text += this.skills[this.skillsInd].charAt(this.i);
+      this.i++;
+      setTimeout(() => this.typeWriter(), this.speed);
+    } else {
+      this.deleteTypeWriter();
+    }
+  }
+
+  deleteTypeWriter() {
+    if(this.i > 0) {
+      this.text = this.text.slice(0,this.text.length - 2)
+      this.i--;
+      setTimeout(() => this.deleteTypeWriter(), this.speed);
+    } else {
+      if(this.skillsInd === this.skills.length - 1) {
+        this.skillsInd = 0;
+      } else {
+        this.skillsInd++;
+      }
+      this.typeWriter();
+    }
   }
 
 }
